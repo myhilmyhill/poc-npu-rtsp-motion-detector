6982715d4455b96b967d0357c64f2038e481fb6a
myhilmyhill <myhilmyhill@outlook.com>
2026-03-11 22:43:05 +0900

    feat: add `simple_demo.ps1` to set up the QAIRT environment and run the QNN demo scripts.

---
+++ scripts/simple_demo.ps1
@@ -0,0 +1,17 @@
+﻿pushd C:\Users\ebyck\qairt\2.44.0.260225\bin
+.\envsetup.ps1 
+& "$env:QAIRT_SDK_ROOT\bin\check-windows-dependency.ps1"
+& "$env:QAIRT_SDK_ROOT\bin\envcheck.ps1" -m
+popd
+
+py -3.10 -m venv "venv"
+& "venv\Scripts\Activate.ps1"
+python -m pip install --upgrade pip
+python "$env:QAIRT_SDK_ROOT\bin\check-python-dependency"
+
+# QNNExecutionProvider が使えないときは以下を実行
+# pip uninstall onnxruntime
+# pip install onnxruntime-qnn
+
+python .\check_qnn.py
+python .\simple_demo.py
