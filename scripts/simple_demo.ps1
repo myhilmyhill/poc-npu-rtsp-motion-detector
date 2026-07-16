b79fc918ef89a9a7d0bc455f861e0c119c186a35
myhilmyhill <myhilmyhill@outlook.com>
2026-07-16 21:10:21 +0900

    feat: add setup and execution script for QNN environment and demo workflow

--- scripts/simple_demo.ps1
+++ scripts/simple_demo.ps1
@@ -1,4 +1,4 @@
-pushd "$env:USERPROFILE\qairt\2.44.0.260225\bin"
+pushd "$env:USERPROFILE\qairt\2.48.0.260626\bin"
 .\envsetup.ps1 
 & "$env:QAIRT_SDK_ROOT\bin\check-windows-dependency.ps1"
 & "$env:QAIRT_SDK_ROOT\bin\envcheck.ps1" -m
